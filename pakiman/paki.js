94c83f916546309959188d0647269c6eed7cc459
adgarboc <adgarboc@gmail.com>
2020-08-15 18:21:28 -0500

    Cuarto proyecto: Pakimanes

--- pakiman/paki.js
+++ pakiman/paki.js
@@ -1,9 +1,20 @@
-class Pakiman
+var imagenes = [];
+imagenes["Cauchin"] = "vaca.png";
+imagenes["Pocacho"] = "pollo.png";
+imagenes["Tocinauro"] = "cerdo.png";
+
+var coleccion = [];
+coleccion.push(new Pakiman("Cauchin", 100, 30));
+coleccion.push(new Pakiman("Pocacho", 80, 50));
+coleccion.push(new Pakiman("Tocinauro", 80, 50));
+
+for (var adancito of coleccion) 
 {
-    constructor()
-    {
-        this.tipo = "tierra" 
-    }
+    adancito.mostrar();
 }
 
-var cauchin = new Pakiman();
+for(var x of coleccion[0])
+{
+    console.log(x);
+}
+
